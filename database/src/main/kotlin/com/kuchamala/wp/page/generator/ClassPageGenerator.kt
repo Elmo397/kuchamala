47cef69e01d0cef7e3f9f286db0d0c22777a016c
Alexander Podkhalyuzin <Alexander.Podkhalyuzin@jetbrains.com>
2020-03-09 18:28:29 +0300

    Math page generator

--- database/src/main/kotlin/com/kuchamala/wp/page/generator/ClassPageGenerator.kt
+++ database/src/main/kotlin/com/kuchamala/wp/page/generator/ClassPageGenerator.kt
@@ -1,16 +1,141 @@
 package com.kuchamala.wp.page.generator
 
-import com.kuchamala.database.entity.EventEntity
-import com.kuchamala.database.entity.TeacherClasses
-import com.kuchamala.database.entity.Teachers
 import com.kuchamala.database.model.ClassesModel
 import com.kuchamala.database.model.TeacherClassesModel
-import com.kuchamala.database.transactions.findTeacher
-import com.kuchamala.database.transactions.findTeacherClass
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.transactions.transaction
+
+fun main() {
+    println(ClassPageGenerator().page())
+}
 
 class ClassPageGenerator {
+
+    fun row(
+        bg_color: String, forColumns: Boolean, separator: Boolean, sectionId: String?, columnHeight: Boolean,
+        data: () -> String
+    ): String {
+        val columns: String =
+            if (forColumns) """ equal_column_height="middle-content" mobile_equal_column_height="false" """
+            else ""
+        val separatorTop: String =
+            if (separator) """ separator_top_size="30px" """
+            else ""
+        val sectionIdString: String =
+            if (sectionId != null) """ section_id="$sectionId" """
+            else ""
+        val columnHeightString: String =
+            if (columnHeight) """ el_wrapper_class="column-height" """
+            else ""
+        return """
+            [vc_row bg_type="color"${columns}${separatorTop}bg_color="#${bg_color}"${sectionIdString}${columnHeightString}]${data()}[/vc_row]
+        """.trimIndent().trim()
+    }
+
+    fun formRow(formId: Int): String {
+        return row("f5f5f5", false, false, "form", false) {
+            """
+            [vc_column width="1/4"][/vc_column][vc_column width="1/2"][contact-form-7 id="$formId"][/vc_column][vc_column width="1/4"][/vc_column]
+        """.trimIndent()
+        }
+    }
+
+    fun teacherRow(teacherHtml: String, teacherName: String, imageId: Int): String {
+        return row("ffffff", true, false, null, false) {
+            """
+            [vc_column width="1/2" el_wrapper_class="column-height"]${image(imageId)}[/vc_column][vc_column width="1/2" el_wrapper_class="column-height"][fildisi_title]Педагог: ${teacherName}[/fildisi_title][vc_column_text css=".vc_custom_1573293625443{margin-bottom: 30px !important;}"]$teacherHtml[/vc_column_text][fildisi_empty_space][fildisi_empty_space height_multiplier="3x"][/vc_column]
+        """.trimIndent()
+        }
+    }
+
+    fun descriptionRow(descriptionText: String, title: String?, imageLeft: Boolean, imageId: Int): String {
+        val titleString =
+            if (title != null) """[fildisi_title heading_tag="h2"]$title[/fildisi_title]"""
+            else ""
+        val imageColumn = """[vc_column width="1/2" tablet_sm_width="1" el_wrapper_class="column-height"]${image(imageId)}[/vc_column]"""
+        val textColumn = """[vc_column width="1/2" tablet_sm_width="1" el_wrapper_class="column-height"]$titleString[vc_column_text css=".vc_custom_1552745539215{margin-bottom: 30px !important;}"]$descriptionText[/vc_column_text][/vc_column]"""
+
+        val columns =
+            if (imageLeft)
+                """
+                    $imageColumn$textColumn
+                """.trimIndent().trim()
+            else
+                """
+                    $textColumn$imageColumn
+                """.trimIndent().trim()
+
+        return row(if (imageLeft) "ffffff" else "f5f5f5", true, false, "description", false) {
+            columns
+        }
+    }
+
+    fun headerRow(title: String, textHtml: String): String {
+        val buttons: String =
+            """
+                <span style="display: inline !important; float: none; background-color: #ffffff; color: #333333; cursor: text; font-family: Georgia,'Times New Roman','Bitstream Charter',Times,serif; font-size: 16px; font-style: normal; font-variant: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-decoration: none; text-indent: 0px; text-transform: none; -webkit-text-stroke-width: 0px; white-space: normal; word-spacing: 0px;">[sc name="join-button"][sc name="more-button"]</span>
+            """.trimIndent().trim()
+
+        return row("f5f5f5", true, true, null, false) {
+            """
+            [vc_column width="1/2" tablet_sm_width="1"][fildisi_title heading_tag="h1"]${title}[/fildisi_title][vc_column_text css=".vc_custom_1552807612682{margin-bottom: 20px !important;}"]$textHtml[/vc_column_text]$buttons[/vc_column][vc_column width="1/2" tablet_sm_width="1"]${image(509)}[/vc_column]
+        """.trimIndent().trim()
+        }
+    }
+
+    fun image(id: Int): String {
+        return """
+            [fildisi_single_image image_mode="large" image="$id" image_full_column="yes"]
+        """.trimIndent().trim()
+    }
+
+    fun page(): String {
+        val timetableAndPrice: String =
+            """
+                Первое занятие — бесплатное. Абонемент — 700 руб/зан. Занятия проходят <a href="http://www.kuchamala.ru/contact/">недалеко</a> от ст. м. Озерки.
+            <h4>Расписание:</h4>
+            4-5 лет: вт. с 17:00 до 18:00
+            5-7 лет: вт. с 18:00 до 19:00
+            7-8 лет: вт. с 15:00 до 16:00/ чт. с 16:00 до 17:00
+            """.trimIndent().trim()
+
+        val title = "Математика для детей"
+
+        val descriptionTitle = "Математические занятия для подготовки ребенка"
+
+        val description1Text: String =
+            """
+                Приглашаем детей от четырех до десяти лет на занятия математикой.
+
+            <strong>Обучение детей математике в нашем центре</strong> обличено в пока еще наиболее понятную для ребенка форму - игры.
+
+            <strong>Авторская методика подготовки детей </strong>нашего педагога, Чивиксиной Наталии Валерьевны, разработана таким образом, что основной упор делается на развитие у детей логики, пространственного мышления и умения работать в команде. Ведь именно это – три кита, на которых базируется не только успешное освоение математики, но и всей школьной программы в целом.
+            """.trimIndent().trim()
+
+        val description2Text: String =
+            """
+                Мы работаем с логическими и настольными играми, головоломками, много внимания уделяется развитию пространственного мышления. Это не математика в привычном виде (арифметика), а скорее обучение на грани с другими предметами, занятия на развитие логики в широком смысле.
+            <h3><style type="text/css"><!--td {border: 1px solid #ccc;}br {mso-data-placement:same-cell;}--></style></h3>
+            <strong>Индивидуальные математические занятия  </strong>
+
+            Нам важна обратная связь с родителями, в конце каждого занятия вы можете пообщаться с педагогом. Наш приоритет - <a href="http://www.kuchamala.ru/classes-softschool/">общий комфорт и результат ребенка</a>. Мы очень любим индивидуальный подход к каждому и уделяем этому время и внимание. Детям будет сложно, но очень интересно!
+
+            Занятия проводятся по авторской методике нашего педагога <a href="https://vk.com/play_learn">Play &amp; Learn</a>.
+            """.trimIndent().trim()
+
+        val teacherHtml: String =
+            """
+                Меня зовут Варвара. Я закончила математико-механический факультет СПбГУ, являюсь кандидатом физико-математических наук.Имею опыт преподавания у студентов, занималась репетиторством со школьниками.
+            Считаю, что развивать свои способности нужно с детства, но обязательно в интересной для ребёнка форме. Будучи мамой двух любознательных мальчишек я знаю, как заинтересовать и направить детскую энергию в нужное русло.
+            """.trimIndent().trim()
+
+        val teacherName: String = "Варвара Феоктистова"
+
+        return headerRow(title, timetableAndPrice) +
+                descriptionRow(description1Text, descriptionTitle, true,491) +
+                descriptionRow(description2Text, null, false, 2633) +
+                teacherRow(teacherHtml, teacherName, 3135) +
+                formRow(266)
+    }
+
     private var rowNumberFlag = 0 // flag for background color in rows
 
     // Some required information to add a post to the Wordpress database
---
+++ database/src/test/kotlin/com/kuchamala/wp/page/generator/ClassPageGeneratorTest.kt
@@ -0,0 +1,30 @@
+package com.kuchamala.wp.page.generator
+
+import org.junit.Test
+
+class ClassPageGeneratorTest {
+
+    @Test
+    fun testMath() {
+        val mathPage: String =
+            """
+            [vc_row bg_type="color" equal_column_height="middle-content" mobile_equal_column_height="false" separator_top_size="30px" bg_color="#f5f5f5"][vc_column width="1/2" tablet_sm_width="1"][fildisi_title heading_tag="h1"]Математика для детей[/fildisi_title][vc_column_text css=".vc_custom_1552807612682{margin-bottom: 20px !important;}"][text-blocks id="math-price"]
+            <h4></h4>
+            <h4>Расписание:</h4>
+            [text-blocks id="math-timetable"][/vc_column_text]<span style="display: inline !important; float: none; background-color: #ffffff; color: #333333; cursor: text; font-family: Georgia,'Times New Roman','Bitstream Charter',Times,serif; font-size: 16px; font-style: normal; font-variant: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-decoration: none; text-indent: 0px; text-transform: none; -webkit-text-stroke-width: 0px; white-space: normal; word-spacing: 0px;">[sc name="join-button"][sc name="more-button"]</span>[/vc_column][vc_column width="1/2" tablet_sm_width="1"][fildisi_single_image image_mode="large" image="509" image_full_column="yes"][/vc_column][/vc_row][vc_row equal_column_height="middle-content" mobile_equal_column_height="false" section_id="description"][vc_column width="1/2" tablet_sm_width="1" el_wrapper_class="column-height"][fildisi_single_image image_mode="large" image="491" image_full_column="yes"][/vc_column][vc_column width="1/2" tablet_sm_width="1" el_wrapper_class="column-height"][fildisi_title heading_tag="h2"]Математические занятия для подготовки ребенка[/fildisi_title][vc_column_text css=".vc_custom_1552745539215{margin-bottom: 30px !important;}"]Приглашаем детей от четырех до десяти лет на занятия математикой.
+
+            <strong>Обучение детей математике в нашем центре</strong> обличено в пока еще наиболее понятную для ребенка форму - игры.
+
+            <strong>Авторская методика подготовки детей </strong>нашего педагога, Чивиксиной Наталии Валерьевны, разработана таким образом, что основной упор делается на развитие у детей логики, пространственного мышления и умения работать в команде. Ведь именно это – три кита, на которых базируется не только успешное освоение математики, но и всей школьной программы в целом.[/vc_column_text][/vc_column][/vc_row][vc_row bg_type="color" equal_column_height="middle-content" mobile_equal_column_height="false" section_id="description" bg_color="#f5f5f5" el_wrapper_class="column-height"][vc_column width="1/2" tablet_sm_width="1" el_wrapper_class="column-height"][vc_column_text css=".vc_custom_1552743235029{margin-bottom: 30px !important;}"]Мы работаем с логическими и настольными играми, головоломками, много внимания уделяется развитию пространственного мышления. Это не математика в привычном виде (арифметика), а скорее обучение на грани с другими предметами, занятия на развитие логики в широком смысле.
+            <h3><style type="text/css"><!--td {border: 1px solid #ccc;}br {mso-data-placement:same-cell;}--></style></h3>
+            <strong>Индивидуальные математические занятия  </strong>
+
+            Нам важна обратная связь с родителями, в конце каждого занятия вы можете пообщаться с педагогом. Наш приоритет - <a href="http://www.kuchamala.ru/classes-softschool/">общий комфорт и результат ребенка</a>. Мы очень любим индивидуальный подход к каждому и уделяем этому время и внимание. Детям будет сложно, но очень интересно!
+
+            Занятия проводятся по авторской методике нашего педагога <a href="https://vk.com/play_learn">Play &amp; Learn</a>.[/vc_column_text][/vc_column][vc_column width="1/2" tablet_sm_width="1" el_wrapper_class="column-height"][fildisi_single_image image_mode="large" image="2633" image_full_column="yes"][/vc_column][/vc_row][vc_row][vc_column width="1/2" el_wrapper_class="column-height"][fildisi_single_image image_mode="large" image="3135" image_full_column="yes"][/vc_column][vc_column width="1/2" el_wrapper_class="column-height"][fildisi_title]Педагог: Варвара Феоктистова[/fildisi_title][vc_column_text css=".vc_custom_1573293625443{margin-bottom: 30px !important;}"]Меня зовут Варвара. Я закончила математико-механический факультет СПбГУ, являюсь кандидатом физико-математических наук.Имею опыт преподавания у студентов, занималась репетиторством со школьниками.
+            Считаю, что развивать свои способности нужно с детства, но обязательно в интересной для ребёнка форме. Будучи мамой двух любознательных мальчишек я знаю, как заинтересовать и направить детскую энергию в нужное русло.[/vc_column_text][fildisi_empty_space][fildisi_empty_space height_multiplier="3x"][/vc_column][/vc_row][vc_row bg_type="color" section_id="form" bg_color="#f5f5f5"][vc_column width="1/4"][/vc_column][vc_column width="1/2"][contact-form-7 id="266"][/vc_column][vc_column width="1/4"][/vc_column][/vc_row]
+        """.trimIndent()
+
+
+    }
+}
